e65b59e233025c1c58c7b2f0f0b36aed6b9e8c7d
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 18:21:31 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
-// next.config.mjs
 const isProd = process.env.NODE_ENV === 'production'
-// ¡Debe coincidir EXACTO con el nombre del repo en GitHub!
 const repo = 'CAFE_PRODUCCIONES'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                 // genera /out para GitHub Pages
+  output: 'export',
   basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : undefined,
-  images: { unoptimized: true },    // necesario con next export
-  trailingSlash: true,              // evita 404 con Pages
+  images: {
+    unoptimized: true, // necesario con next export
+  },
+  trailingSlash: true,
 }
 
 export default nextConfig

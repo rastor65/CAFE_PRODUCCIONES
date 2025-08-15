995d41729ea1447756e0a11d6896f7f81cd073c2
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 17:37:39 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,6 @@ export default {
   output: 'export',              // genera /out automáticamente
   images: { unoptimized: true }, // evitar optimización en server
   // basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
+  // assetPrefix: isProd ? `/${repo}/` : '',
   trailingSlash: true,
 };

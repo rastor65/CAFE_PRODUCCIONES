0b701a7a716a67c10f333f8636858ba36732d88f
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-15 18:31:45 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 const repo = 'CAFE_PRODUCCIONES'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   output: 'export',
   basePath: isProd ? `/${repo}` : '',
-  images: {
-    unoptimized: true, // necesario con next export
-  },
+  images: { unoptimized: true },
   trailingSlash: true,
 }
-
-export default nextConfig
